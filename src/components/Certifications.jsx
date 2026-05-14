aaad24c01060ee171a9eb6bae27c0a12b7cfee5b
Calderon Cruces Jose Ayrton <u21215967@utp.edu.pe>
2026-05-13 23:38:17 -0500

    Agregando Grado de Bachiller a certificaciones 2 xd

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -14,10 +14,10 @@ const Certifications = () => {
       title: "Bachiller en Ingeniería de Sistemas e Informática",
       issuer: "Universidad Tecnológica del Perú",
       date: "2026",
-      image: publicCertf("DIPLOMA BACHILLER_CALDERÓN__Página_1.jpg"),
+      image: publicCertf("DIPLOMABACHILLER_CALDERÓN__Página_1.jpg"),
       images: [
-        publicCertf("DIPLOMA BACHILLER_CALDERÓN__Página_1.jpg"),
-        publicCertf("DIPLOMA BACHILLER_CALDERÓN__Página_2.jpg"),
+        publicCertf("DIPLOMABACHILLER_CALDERÓN__Página_1.jpg"),
+        publicCertf("DIPLOMABACHILLER_CALDERÓN__Página_2.jpg"),
       ],
       link: "#"
     },
